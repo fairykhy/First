d6a5f537c87497d5770851441d529563f48561f3
fairykhy <fairykhy02@gmail.com>
2023-07-04 04:34:43 +0900

    1st commit (230704 04:34)

---
+++ app/src/main/java/cs/kaist/first/DetectText.kt
@@ -0,0 +1,83 @@
+package cs.kaist.first
+
+import android.content.Context
+import android.os.Environment
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.cloud.vision.v1.AnnotateImageRequest
+import com.google.cloud.vision.v1.AnnotateImageResponse
+import com.google.cloud.vision.v1.BatchAnnotateImagesResponse
+import com.google.cloud.vision.v1.Feature
+import com.google.cloud.vision.v1.Image
+import com.google.cloud.vision.v1.ImageAnnotatorClient
+import com.google.cloud.vision.v1.ImageAnnotatorSettings
+import com.google.protobuf.ByteString
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.FileInputStream
+import java.io.IOException
+
+object DetectText {
+    @Throws(IOException::class)
+    fun detectText(context: Context) {
+        // TODO(developer): Replace these variables before running the sample.
+        val directory = Environment.getExternalStorageDirectory().absolutePath
+        val filePath = "$directory/Download/30004_craw1.jpg"
+//        val filePath = "C:/Users/SAMSUNG/Desktop/receipt1.jpg"/
+        val scope = CoroutineScope(Dispatchers.Main)
+
+// 코루틴을 실행합니다.
+        scope.launch {
+            // detectText() 메서드를 호출합니다.
+            detectTextAsync(filePath, context)
+        }
+    }
+
+    // Detects text in the specified image.
+    @Throws(IOException::class)
+    suspend fun detectText(filePath: String?, context: Context) {
+        val credentials: GoogleCredentials = GoogleCredentials.fromStream(
+            context.resources.openRawResource(R.raw.madcamp_first)
+        )
+        val scopedCredentials = credentials.createScoped("https://www.googleapis.com/auth/cloud-platform")
+
+        val requests: MutableList<AnnotateImageRequest> = ArrayList<AnnotateImageRequest>()
+        val imgBytes: ByteString = ByteString.readFrom(FileInputStream(filePath))
+        val img: Image = Image.newBuilder().setContent(imgBytes).build()
+        val feat: Feature = Feature.newBuilder().setType(Feature.Type.TEXT_DETECTION).build()
+        val request: AnnotateImageRequest =
+            AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build()
+        requests.add(request)
+
+        ImageAnnotatorClient.create(ImageAnnotatorSettings.newBuilder().setCredentialsProvider {
+            scopedCredentials
+        }.build()).use { client ->
+            val response: BatchAnnotateImagesResponse = client.batchAnnotateImages(requests)
+            val responses: List<AnnotateImageResponse> = response.getResponsesList()
+            for (res in responses) {
+                if (res.hasError()) {
+                    System.out.format("Error: %s%n", res.getError().getMessage())
+                    return
+                }
+
+                // For full list of available annotations, see http://g.co/cloud/vision/docs
+                for (annotation in res.getTextAnnotationsList()) {
+                    System.out.format("Text: %s%n", annotation.getDescription())
+                    System.out.format("Position : %s%n", annotation.getBoundingPoly())
+                }
+            }
+        }
+    }
+
+    fun detectTextAsync(filePath: String?, context: Context) {
+        val scope = CoroutineScope(Dispatchers.IO)
+
+        scope.launch {
+            try {
+                detectText(filePath, context)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
--- app/src/main/java/cs/kaist/first/ReceiptFragment.kt
+++ app/src/main/java/cs/kaist/first/ReceiptFragment.kt
@@ -40,88 +40,108 @@ class ReceiptFragment : Fragment() {
         val runOCRTextView = receiptView.findViewById<TextView>(R.id.runOCRTextView)
         OCRTextView = receiptView.findViewById(R.id.OCRTextView)
 
-        image = BitmapFactory.decodeResource(resources, R.drawable.receipt1)
-        val receiptImageView = receiptView.findViewById<ImageView>(R.id.imageView)
-        receiptImageView.setImageBitmap(image)
-
-        // 언어파일 경로
-        datapath = requireContext().getFilesDir().toString() + "/tesseract/"
-
-        Log.d("datapath",datapath)
-        checkFile(File(datapath + "tessdata/"))
-
-        //Tesseract API 언어 세팅
-        val lang = "kor"
-
-        mTess = TessBaseAPI()
-        mTess!!.init(datapath,lang)
-        runOCRTextView.setOnClickListener {
-            processImage(receiptView)
-        }
-
-        return receiptView
-    }
-    fun processImage(view: View?) {
-        var OCRresult: String? = null
-        mTess!!.setImage(image)
-        OCRresult = mTess!!.utF8Text
-        OCRTextView!!.text = OCRresult
-        println(OCRresult[0])
-    }
-
-    private val langFileName = "kor.traineddata"
-    private fun copyFiles() {
-        try {
-            val filepath = datapath + "tessdata/" + langFileName
-            val assetManager = requireActivity().assets
-            val instream: InputStream = assetManager.open(langFileName)
-            val outstream: OutputStream = FileOutputStream(filepath)
-            val buffer = ByteArray(1024)
-            var read: Int
-            while (instream.read(buffer).also { read = it } != -1) {
-                outstream.write(buffer, 0, read)
-            }
-            outstream.flush()
-            outstream.close()
-            instream.close()
-        } catch (e: FileNotFoundException) {
-            e.printStackTrace()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-    }
-
-    fun checkFile(dir: File) {
-        //디렉토리가 없으면 디렉토리를 만들고 그후에 파일을 카피
-        if (!dir.exists() && dir.mkdirs()) {
-            copyFiles()
-        }
-        //디렉토리가 있지만 파일이 없으면 파일카피 진행
-        if (dir.exists()) {
-            val datafilepath = datapath + "tessdata/" + langFileName
-            val datafile = File(datafilepath)
-            if (!datafile.exists()) {
-                copyFiles()
-            }
-        }
-    }
-
-    fun ReceiptParsing (text: String) {
-        //매장명
-        val name_arr = arrayListOf<String>("매장", "매장명", "상호", "상호명", "가맹점", "가맹점명")
-        val total = arrayListOf<String>("합계", "합계금액", "총액", "판매금액", "결제금액")
-        for(x: String in name_arr){
-            if(text.contains(x)){
-                val idx = text.indexOf(x)
-                val store_name = text[idx+1].toString()
-            }
-        }
-//        for(y: String in total){
-//            if(text.contains(y)){
-//                val idx =
+//        image = BitmapFactory.decodeResource(resources, R.drawable.sample1)
+//        val receiptImageView = receiptView.findViewById<ImageView>(R.id.imageView)
+//        receiptImageView.setImageBitmap(image)
+        DetectText.detectText(requireContext())
+//        thread{
+//            val url = URL("https://vision.googleapis.com/v1/images:annotate")
+////            val postData = "foo1=bar1&foo2=bar2"
+//
+//            val conn = url.openConnection()
+//            conn.doOutput = true
+//            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
+//            conn.setRequestProperty("Content-Length", postData.length.toString())
+//
+//            DataOutputStream(conn.getOutputStream()).use { it.writeBytes(postData) }
+//            BufferedReader(InputStreamReader(conn.getInputStream())).use { bf ->
+//                var line: String?
+//                while (bf.readLine().also { line = it } != null) {
+//                    println(line)
+//                }
 //            }
 //        }
 
+//        // 언어파일 경로
+//        datapath = requireContext().getFilesDir().toString() + "/tesseract/"
+//
+//        Log.d("datapath",datapath)
+//        checkFile(File(datapath + "tessdata/"))
+//
+//        //Tesseract API 언어 세팅
+//        val lang = "kor"
+//
+//        mTess = TessBaseAPI()
+//        mTess!!.init(datapath,lang)
+//        runOCRTextView.setOnClickListener {
+//            processImage(receiptView)
+//        }
+//
+//        return receiptView
+//    }
+//    fun processImage(view: View?) {
+//        var OCRresult: String? = null
+//        mTess!!.setImage(image)
+//        OCRresult = mTess!!.utF8Text
+//        OCRTextView!!.text = OCRresult
+//        println(OCRresult[0])
+//    }
+//
+//    private val langFileName = "kor.traineddata"
+//    private fun copyFiles() {
+//        try {
+//            val filepath = datapath + "tessdata/" + langFileName
+//            val assetManager = requireActivity().assets
+//            val instream: InputStream = assetManager.open(langFileName)
+//            val outstream: OutputStream = FileOutputStream(filepath)
+//            val buffer = ByteArray(1024)
+//            var read: Int
+//            while (instream.read(buffer).also { read = it } != -1) {
+//                outstream.write(buffer, 0, read)
+//            }
+//            outstream.flush()
+//            outstream.close()
+//            instream.close()
+//        } catch (e: FileNotFoundException) {
+//            e.printStackTrace()
+//        } catch (e: IOException) {
+//            e.printStackTrace()
+//        }
+//    }
+//
+//    fun checkFile(dir: File) {
+//        //디렉토리가 없으면 디렉토리를 만들고 그후에 파일을 카피
+//        if (!dir.exists() && dir.mkdirs()) {
+//            copyFiles()
+//        }
+//        //디렉토리가 있지만 파일이 없으면 파일카피 진행
+//        if (dir.exists()) {
+//            val datafilepath = datapath + "tessdata/" + langFileName
+//            val datafile = File(datafilepath)
+//            if (!datafile.exists()) {
+//                copyFiles()
+//            }
+//        }
+//    }
+
+//    fun ReceiptParsing (text: String) {
+//        val text_arr = text.split(" ")
+//        //매장명
+//        val name_arr = arrayListOf<String>("매장", "매장명", "상호", "상호명", "가맹점", "가맹점명")
+//        val total = arrayListOf<String>("합계", "합계금액", "총액", "판매금액", "결제금액")
+//        for(x: String in name_arr){
+//            if(text_arr.contains(x)){
+//                val idx = text_arr.indexOf(x)
+//                val store_name = text_arr[idx+1].toString()
+//            }
+//        }
+////        for(y: String in total){
+////            if(text.contains(y)){
+////                val idx =
+////            }
+//        }
+        return  null
+//
     }
 
 
